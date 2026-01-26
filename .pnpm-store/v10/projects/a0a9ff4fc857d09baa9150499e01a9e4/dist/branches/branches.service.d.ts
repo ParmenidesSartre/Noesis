b06febfb3ed075ecdee68cf26f4bc5654c97d341
ParmenidesSartre <faizalazman88@gmail.com>
2026-01-26 21:28:14 +0800

    chore: remove .pnpm-store from repository and add to gitignore

--- .pnpm-store/v10/projects/a0a9ff4fc857d09baa9150499e01a9e4/dist/branches/branches.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateBranchDto } from './dto/create-branch.dto';
-import { UpdateBranchDto } from './dto/update-branch.dto';
-export declare class BranchesService {
-    create(createBranchDto: CreateBranchDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateBranchDto: UpdateBranchDto): string;
-    remove(id: number): string;
-}
